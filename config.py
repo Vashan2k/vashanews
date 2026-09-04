22638ee2b903d1b23b9cc15c2da758a5f9f0e5de
Vashan2k <vashan2k@ya.ru>
2026-09-05 02:50:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 """
 Конфигурация для Render.com
 """
-import os  # ← ЭТОЙ СТРОКИ НЕ ХВАТАЛО!
+import os
 
 # --- Telegram User API ---
 TG_API_ID = int(os.environ.get("TG_API_ID", "0"))
@@ -15,7 +15,7 @@ TG_REPORT_CHAT_ID = os.environ.get("TG_REPORT_CHAT_ID", "")
 
 # --- Qwen Code API (DashScope) ---
 DASHSCOPE_API_KEY = os.environ.get("DASHSCOPE_API_KEY", "")
-QWEN_MODEL = os.environ.get("QWEN_MODEL", "qwen3-coder-plus")
+QWEN_MODEL = os.environ.get("QWEN_MODEL", "qwen-turbo")  # бесплатная модель
 QWEN_BASE_URL = os.environ.get("QWEN_BASE_URL", "https://dashscope-intl.aliyuncs.com/compatible-mode/v1")
 
 # --- Файлы ---
